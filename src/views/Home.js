0ca1bda8a6b207d9e53669ebe88f20146c877bf3
Jonathan <warkentin.jon@gmail.com>
2019-08-27 22:32:45 -0500

    add extra package semantic-ui-css to make semantic ui work properly. remove all content from App.css and index.css for a clean slate. test buttons, add component folder

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import ButtonExampleEmphasis from '../components/ButtonExampleEmphasis';
 
-function Home () {
-    return(
-        <div>This is the home page!</div>
+function Home() {
+    return (
+        <div>
+            <p>This is the home page!</p>
+            <ButtonExampleEmphasis />
+        </div>
     );
 }
 
