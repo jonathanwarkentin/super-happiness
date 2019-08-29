fa4a9866e890e3194c5aa23ea7e60d149fc0a280
Jonathan <warkentin.jon@gmail.com>
2019-08-29 17:01:51 -0500

    simplify NavBar / remove activeState functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,56 +2,34 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { Menu } from 'semantic-ui-react'
 
-export default class NavBar extends React.Component {
-    state = {}
+const NavBar = () => {
+    return (
+        <Menu>
+            <Link to="/">
+                <Menu.Item>
+                    Home
+                </Menu.Item>
+            </Link>
 
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+            <Link to="/exec">
+                <Menu.Item>
+                    E-Board
+                </Menu.Item>
+            </Link>
 
-    render() {
-        const { activeItem } = this.state
+            <Link to="/events">
+                <Menu.Item>
+                    Events
+                </Menu.Item>
+            </Link>
 
-        return (
-            <Menu>
-                <Link to="/">
-                    <Menu.Item
-                        name='home'
-                        active={activeItem === 'home'}
-                        onClick={this.handleItemClick}
-                    >
-                        Home
-                    </Menu.Item>
-                </Link>
-
-                <Link to="/exec">
-                    <Menu.Item
-                        name='exec'
-                        active={activeItem === 'exec'}
-                        onClick={this.handleItemClick}
-                    >
-                        E-Board
-                    </Menu.Item>
-                </Link>
-
-                <Link to="/events">
-                    <Menu.Item
-                        name='events'
-                        active={activeItem === 'events'}
-                        onClick={this.handleItemClick}
-                    >
-                        Events
-                    </Menu.Item>
-                </Link>
-
-                <Link to="contact">
-                    <Menu.Item
-                        name='contact'
-                        active={activeItem === 'contact'}
-                        onClick={this.handleItemClick}
-                    >
-                        Contact
-                    </Menu.Item>
-                </Link>
-            </Menu>
-        );
-    }
+            <Link to="contact">
+                <Menu.Item>
+                    Contact
+                </Menu.Item>
+            </Link>
+        </Menu>
+    );
 }
+
+export default NavBar
