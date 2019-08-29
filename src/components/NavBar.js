f4f11b2023e41c17dffd5aa840f59826ca207fb8
Jonathan <warkentin.jon@gmail.com>
2019-08-29 16:49:40 -0500

    semantic-ify the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,57 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { Menu } from 'semantic-ui-react'
 
-const NavBar = () => {
-    return (
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/exec">Exec</Link>
-                </li>
-                <li>
-                    <Link to="/events">Events</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </div>);
+export default class NavBar extends React.Component {
+    state = {}
+
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return (
+            <Menu>
+                <Link to="/">
+                    <Menu.Item
+                        name='home'
+                        active={activeItem === 'home'}
+                        onClick={this.handleItemClick}
+                    >
+                        Home
+                    </Menu.Item>
+                </Link>
+
+                <Link to="/exec">
+                    <Menu.Item
+                        name='exec'
+                        active={activeItem === 'exec'}
+                        onClick={this.handleItemClick}
+                    >
+                        E-Board
+                    </Menu.Item>
+                </Link>
+
+                <Link to="/events">
+                    <Menu.Item
+                        name='events'
+                        active={activeItem === 'events'}
+                        onClick={this.handleItemClick}
+                    >
+                        Events
+                    </Menu.Item>
+                </Link>
+
+                <Link to="contact">
+                    <Menu.Item
+                        name='contact'
+                        active={activeItem === 'contact'}
+                        onClick={this.handleItemClick}
+                    >
+                        Contact
+                    </Menu.Item>
+                </Link>
+            </Menu>
+        );
+    }
 }
-
-export default NavBar
