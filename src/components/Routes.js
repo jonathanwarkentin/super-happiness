3abc8142f00d570164d852ff0197adc312fe4815
Jonathan <warkentin.jon@gmail.com>
2019-08-29 16:31:17 -0500

    add basic nav bar

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch, Link } from "react-router-dom"
 import Home from "../views/Home";
 import Events from "../views/Events";
 import Exec from "../views/Exec";
@@ -9,6 +9,20 @@ import NotFound from "../views/NotFound";
 const Routes = () => {
     return (
         <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/exec">Exec</Link>
+                </li>
+                <li>
+                    <Link to="/events">Events</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/exec" component={Exec} />
