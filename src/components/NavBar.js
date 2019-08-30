767bf0a6d5c6e776b1454a479d7ff48fd2ffcac6
Jonathan <warkentin.jon@gmail.com>
2019-08-29 21:49:09 -0500

    fix favicon and add bg image

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,31 +4,29 @@ import { Menu } from 'semantic-ui-react'
 import './NavBar.css'
 import logo from '../img/logo.svg'
 
-console.log(logo);
-
 const NavBar = () => {
     return (
         <Menu id="navbar" size="massive">
             <Link to="/">
-                <Menu.Item link id="home">
+                <Menu.Item link>
                     <img id="logo" src={logo} alt="DON'T PANIC! Logo" />
                 </Menu.Item>
             </Link>
 
             <Link to="/exec">
-                <Menu.Item class="menu-item" link>
+                <Menu.Item link>
                     E-Board
                 </Menu.Item>
             </Link>
 
             <Link to="/events">
-                <Menu.Item class="menu-item" link>
+                <Menu.Item link>
                     Events
                 </Menu.Item>
             </Link>
 
             <Link to="contact">
-                <Menu.Item class="menu-item" link>
+                <Menu.Item link>
                     Contact
                 </Menu.Item>
             </Link>
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from "./components/Routes"
 import { Router } from "react-router-dom"
-import createHistory from "history/createBrowserHistory"
+import { createBrowserHistory } from 'history'
 import 'semantic-ui-css/semantic.min.css'
-const history = createHistory()
+const history = createBrowserHistory()
 
 ReactDOM.render(
     <Router history={history}>
