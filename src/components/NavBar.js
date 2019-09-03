80dacc8f497e3341e76832d30003bd678eee8657
Jonathan <warkentin.jon@gmail.com>
2019-09-03 09:23:03 -0500

    add menu items to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,25 @@
 import React from "react"
+import { Menu } from "semantic-ui-react"
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>NAVBAR</h1>
-        </div>
+        <Menu borderless widths={5}>
+            <Menu.Item>
+                Home
+            </Menu.Item>
+            <Menu.Item>
+                About
+            </Menu.Item>
+            <Menu.Item>
+                Events
+            </Menu.Item>
+            <Menu.Item>
+                Exec
+            </Menu.Item>
+            <Menu.Item>
+                Contact
+            </Menu.Item>
+        </Menu >
     );
 }
 
