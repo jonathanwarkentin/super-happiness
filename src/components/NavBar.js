c771c28d2fa1d39f4a63579dea9814d39e65abcc
Jonathan <warkentin.jon@gmail.com>
2019-09-06 18:15:25 -0500

    fix nav bar spacing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,46 +1,37 @@
 import React from "react"
-import { Menu, Header } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 import logo from "../img/logo.png"
 import "./NavBar.css"
 
-const navStyle = {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    borderWidth: '0px',
-    boxShadow: 'none'
-}
-
 const NavBar = () => {
     return (
-        <Menu borderless style={navStyle}>
-            <Menu.Item>
-                <Link to="/">
-                    <img id="logo" src={logo} alt="DON'T PANIC! Logo" />
+        <div id="nav-bar">
+
+            <Link to="/">
+                <img id="logo" src={logo} alt="DON'T PANIC! Logo" />
+            </Link>
+
+
+            <Link className='nav-item' to="/about">
+                ABOUT
                 </Link>
-            </Menu.Item>
-            <Menu.Item>
-                <Link className='nav-item' to="/about">
-                    ABOUT
+
+
+            <Link className='nav-item' to="/events">
+                EVENTS
                 </Link>
-            </Menu.Item>
-            <Menu.Item>
-                <Link className='nav-item' to="/events">
-                    EVENTS
+
+
+            <Link className='nav-item' to="/exec">
+                EXECUTIVE BOARD
                 </Link>
-            </Menu.Item>
-            <Menu.Item>
-                <Link className='nav-item' to="/exec">
-                    EXECUTIVE BOARD
+
+
+            <Link className='nav-item' to="/contact">
+                CONTACT
                 </Link>
-            </Menu.Item>
-            <Menu.Item>
-                <Link className='nav-item' to="/contact">
-                    CONTACT
-                </Link>
-            </Menu.Item>
-        </Menu >
+
+        </div >
     );
 }
 
