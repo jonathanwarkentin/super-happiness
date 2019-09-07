4e7fd8b800ee2f0b89553426b79dd2a6fa963da5
Jonathan <warkentin.jon@gmail.com>
2019-09-06 22:12:06 -0500

    exec page layout complete with Lots of Lucas

--- src/views/Exec.js
+++ src/views/Exec.js
@@ -1,10 +1,81 @@
 import React from 'react';
+import { Grid, Image, Card } from 'semantic-ui-react'
+import './Exec.css'
+import lucas from '../img/Lucas.jpg'
 
 export default class Exec extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Executive Board page under construction.</h1>
+            <div id='exec-div'>
+                <h1 id='exec-title'>EXECUTIVE BOARD</h1>
+                <Grid id='exec-grid'>
+                    <Grid.Row className='grid-row' columns={4}>
+                        <Grid.Column className='board-member'>
+                            <Card className='member-card'>
+                                <Image className='headshot' src={lucas} />
+                                <Card.Content className='member-card'>
+                                    <Card.Header className='member-name'>Lucas Perez</Card.Header>
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column className='board-member'>
+                            <Card className='member-card'>
+                                <Image className='headshot' src={lucas} />
+                                <Card.Content className='member-card'>
+                                    <Card.Header className='member-name'>Lucas Perez</Card.Header>
+
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column className='board-member'>
+                            <Card className='member-card'>
+                                <Image className='headshot' src={lucas} />
+                                <Card.Content className='member-card'>
+                                    <Card.Header className='member-name'>Lucas Perez</Card.Header>
+
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column className='board-member'>
+                            <Card className='member-card'>
+                                <Image className='headshot' src={lucas} />
+                                <Card.Content className='member-card'>
+                                    <Card.Header className='member-name'>Lucas Perez</Card.Header>
+
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row className='grid-row' columns={3}>
+                        <Grid.Column className='board-member'>
+                            <Card className='member-card'>
+                                <Image className='headshot' src={lucas} />
+                                <Card.Content className='member-card'>
+                                    <Card.Header className='member-name'>Lucas Perez</Card.Header>
+
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column className='board-member'>
+                            <Card className='member-card'>
+                                <Image className='headshot' src={lucas} />
+                                <Card.Content className='member-card'>
+                                    <Card.Header className='member-name'>Lucas Perez</Card.Header>
+
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                        <Grid.Column className='board-member'>
+                            <Card className='member-card'>
+                                <Image className='headshot' src={lucas} />
+                                <Card.Content className='member-card'>
+                                    <Card.Header className='member-name'>Lucas Perez</Card.Header>
+
+                                </Card.Content>
+                            </Card>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </div>
         )
     }
