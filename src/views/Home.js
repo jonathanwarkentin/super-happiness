ad1fa7cf201cb758762cf0ee02f535960aadf57f
Jonathan <warkentin.jon@gmail.com>
2019-09-14 16:17:38 -0500

    think i fixed home page for landscape phones, google inspector being weird

--- src/views/Home.js
+++ src/views/Home.js
@@ -9,12 +9,12 @@ export default class Contact extends React.Component {
                 <div>
                     <span id='welcome-to'>WELCOME TO</span>
                     <br />
-                    <br />
+                    {/* <Responsive as='br' minAspect={16 / 9} /> */}
                     <span id='loyola'>LOYOLA  </span><span id='university'>UNIVERSITY</span>
-                    <Responsive as='br' {...Responsive.onlyComputer} />
+                    {/* <Responsive as='br' minAspect={16 / 9} /> */}
                     <br />
                     <span id='chicago-coding'><span id='chicago'>CHICAGO'S</span><span id='coding'>CODING</span></span>
-                    <Responsive as='br' {...Responsive.onlyComputer} />
+                    <Responsive as='br' minAspect={16 / 9} />
                     <span id='community'>COMMUNITY</span>
                     <br />
                     <span id='byte'>We might just byte you, though. Got it? Byte. Ha haha hahahaha.</span>
