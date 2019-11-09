9e76f49ff0b10fc11dfba16f1c7e56436750147c
Jonathan <warkentin.jon@gmail.com>
2019-11-09 17:47:48 -0600

    try again

--- src/views/Exec.js
+++ src/views/Exec.js
@@ -65,37 +65,39 @@ export default class Exec extends React.Component {
                     </Grid.Row>
                 </Responsive>
 
-                <Responsive {...Responsive.onlyMobile} as='div' id='exec-div-mobile'>
-                    <Card className='member-card'>
-                        <Image className='headshot' src={martin} />
-                        <Card.Content className='member-card'>
-                            <Card.Header className='member-name'>Martin Zugschwert</Card.Header>
-                        </Card.Content>
-                    </Card>
-                    <Card className='member-card'>
-                        <Image className='headshot' src={linette} />
-                        <Card.Content className='member-card'>
-                            <Card.Header className='member-name'>Linette Maliakal</Card.Header>
-                        </Card.Content>
-                    </Card>
-                    <Card className='member-card'>
-                        <Image className='headshot' src={makenna} />
-                        <Card.Content className='member-card'>
-                            <Card.Header className='member-name'>Makenna Walsh</Card.Header>
-                        </Card.Content>
-                    </Card>
-                    <Card className='member-card'>
-                        <Image className='headshot' src={jonathan} />
-                        <Card.Content className='member-card'>
-                            <Card.Header className='member-name'>Jonathan Warkentin</Card.Header>
-                        </Card.Content>
-                    </Card>
-                    <Card className='member-card'>
-                        <Image className='headshot' src={paulina} />
-                        <Card.Content className='member-card'>
-                            <Card.Header className='member-name'>Paulina Adamski</Card.Header>
-                        </Card.Content>
-                    </Card>
+                <Responsive {...Responsive.onlyMobile} as='div'>
+                    <div id='exec-div-mobile'>
+                        <Card className='member-card'>
+                            <Image className='headshot' src={martin} />
+                            <Card.Content className='member-card'>
+                                <Card.Header className='member-name'>Martin Zugschwert</Card.Header>
+                            </Card.Content>
+                        </Card>
+                        <Card className='member-card'>
+                            <Image className='headshot' src={linette} />
+                            <Card.Content className='member-card'>
+                                <Card.Header className='member-name'>Linette Maliakal</Card.Header>
+                            </Card.Content>
+                        </Card>
+                        <Card className='member-card'>
+                            <Image className='headshot' src={makenna} />
+                            <Card.Content className='member-card'>
+                                <Card.Header className='member-name'>Makenna Walsh</Card.Header>
+                            </Card.Content>
+                        </Card>
+                        <Card className='member-card'>
+                            <Image className='headshot' src={jonathan} />
+                            <Card.Content className='member-card'>
+                                <Card.Header className='member-name'>Jonathan Warkentin</Card.Header>
+                            </Card.Content>
+                        </Card>
+                        <Card className='member-card'>
+                            <Image className='headshot' src={paulina} />
+                            <Card.Content className='member-card'>
+                                <Card.Header className='member-name'>Paulina Adamski</Card.Header>
+                            </Card.Content>
+                        </Card>
+                    </div>
                 </Responsive>
                 <br />
             </div>
