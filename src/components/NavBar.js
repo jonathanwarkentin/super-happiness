a01fb1ccc8f98f7effd45208f3ea5f739efb5b3a
Jonathan <warkentin.jon@gmail.com>
2019-11-09 21:22:22 -0600

    fix mobile nav bar dimensions and make it close on click link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,48 +5,70 @@ import "./NavBar.css"
 import { slide as Menu } from "react-burger-menu"
 import { Responsive } from 'semantic-ui-react'
 
-const NavBar = () => {
-    return (
-        <div>
-            <Responsive as='div' minWidth={Responsive.onlyTablet.minWidth} id="nav-bar">
-                <Link to="/">
-                    <img id="logo" src={logo} alt="DON'T PANIC! Logo" />
-                </Link>
-                <Link className='nav-item' to="/about">
-                    ABOUT
-                </Link>
-                <Link className='nav-item' to="/events">
-                    EVENTS
-                </Link>
-                <Link className='nav-item' to="/exec">
-                    EXECUTIVE BOARD
-                </Link>
-                <Link className='nav-item' to="/contact">
-                    CONTACT
-                </Link>
-            </Responsive>
-            <Responsive {...Responsive.onlyMobile} as='div' id='mobile-nav'>
-                <img id="logo" src={logo} alt="DON'T PANIC! Logo" />
-                <Menu right>
-                    <Link className='nav-item' to="/">
-                        HOME
+class NavBar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+    handleStateChange(state) {
+        this.setState({ menuOpen: state.isOpen })
+    }
+
+    closeMenu() {
+        this.setState({ menuOpen: false })
+    }
+
+    toggleMenu() {
+        this.setState(state => ({ menuOpen: !state.menuOpen }))
+    }
+
+    render() {
+        return (
+            <div>
+                <Responsive as='div' minWidth={Responsive.onlyTablet.minWidth} id="nav-bar">
+                    <Link to="/">
+                        <img id="logo" src={logo} alt="DON'T PANIC! Logo" />
                     </Link>
                     <Link className='nav-item' to="/about">
                         ABOUT
-                    </Link>
+                </Link>
                     <Link className='nav-item' to="/events">
                         EVENTS
-                    </Link>
+                </Link>
                     <Link className='nav-item' to="/exec">
                         EXECUTIVE BOARD
-                    </Link>
+                </Link>
                     <Link className='nav-item' to="/contact">
                         CONTACT
-                    </Link>
-                </Menu>
-            </Responsive>
-        </div>
-    );
+                </Link>
+                </Responsive>
+                <Responsive {...Responsive.onlyMobile} as='div' id='mobile-nav'>
+                    <img id="logo" src={logo} alt="DON'T PANIC! Logo" />
+                    <Menu right isOpen={this.state.menuOpen}
+                        onStateChange={(state) => this.handleStateChange(state)}>
+                        <Link onClick={() => this.closeMenu()} className='nav-item' to="/">
+                            HOME
+                        </Link>
+                        <Link onClick={() => this.closeMenu()} className='nav-item' to="/about">
+                            ABOUT
+                        </Link>
+                        <Link onClick={() => this.closeMenu()} className='nav-item' to="/events">
+                            EVENTS
+                        </Link>
+                        <Link onClick={() => this.closeMenu()} className='nav-item' to="/exec">
+                            EXECUTIVE BOARD
+                        </Link>
+                        <Link onClick={() => this.closeMenu()} className='nav-item' to="/contact">
+                            CONTACT
+                        </Link>
+                    </Menu>
+                </Responsive>
+            </div>
+        )
+    }
 }
 
 export default NavBar
