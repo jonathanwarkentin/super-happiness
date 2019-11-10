d06dc8c8303967bb2f5d0cb7b83ca31a4c6205ac
Jonathan <warkentin.jon@gmail.com>
2019-11-09 23:21:10 -0600

    reformat home page on mobile

--- src/views/Home.js
+++ src/views/Home.js
@@ -6,17 +6,33 @@ export default class Home extends React.Component {
     render() {
         return (
             <div id='home-text'>
-                <div>
+                <Responsive minWidth={Responsive.onlyTablet.minWidth} as='div' id='desktop-home-div'>
                     <span id='welcome-to'>WELCOME TO</span>
                     <br />
-                    {/* <Responsive as='br' minAspect={16 / 9} /> */}
                     <span id='loyola'>LOYOLA  </span><span id='university'>UNIVERSITY</span>
-                    {/* <Responsive as='br' minAspect={16 / 9} /> */}
                     <br />
                     <span id='chicago-coding'><span id='chicago'>CHICAGO'S</span><span id='coding'>CODING</span></span>
-                    <Responsive as='br' {...Responsive.onlyMobile} />
                     <span id='community'>COMMUNITY</span>
-                </div>
+                </Responsive>
+                <Responsive {...Responsive.onlyMobile} as='div' id='mobile-home-div'>
+                    <div>
+                        <span id='welcome-to'>WELCOME TO</span>
+                        <br />
+                    </div>
+                    <div>
+                        <span id='loyola'>LOYOLA</span>
+                        <br />
+                        <span id='university'>UNIVERSITY</span>
+                        <br />
+                        <span id='chicago'>CHICAGO'S</span>
+                        <br />
+                    </div>
+                    <div>
+                        <span id='coding'>CODING</span>
+                        <br />
+                        <span id='community'>COMMUNITY</span>
+                    </div>
+                </Responsive>
             </div>
         )
     }
