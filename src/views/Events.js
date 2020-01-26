82bdfa69decefc7848b057439c14af19ae3d51aa
Jonathan <warkentin.jon@gmail.com>
2020-01-25 18:09:30 -0600

    make event lists show up properly on mobile

--- src/views/Events.js
+++ src/views/Events.js
@@ -63,42 +63,42 @@ export default class Events extends React.Component {
                 year: 2019,
             },
             {
-                loc: "Wednesday, January 22 from 7-8pm in Doyle Hall 001",
+                loc: "Wednesday, January 22 from 7-8pm in Cuneo",
                 description: "Intro meeting. Say hello, get to know people. If you have no idea what is going on come to this and learn a bit about us!",
                 day: 22,
                 month: 1,
                 year: 2020,
             },
             {
-                loc: "Wednesday, January 29 from 7-8pm in Doyle Hall 001",
+                loc: "Wednesday, January 29 from 7-8pm in Cuneo",
                 description: "Chill. A relaxed meeting where you can come and work on any computer science projects that you might want to, try your hand at a coding challenge or two, and most importantly spend some time with your peers!",
                 day: 29,
                 month: 1,
                 year: 2020,
             },
             {
-                loc: "Wednesday, February 5 from 7-8pm in Doyle Hall 001",
+                loc: "Wednesday, February 5 from 7-8pm in Cuneo",
                 description: "What is Computer Science? At this event we will look at what careers lie under the broad term \"Computer Science\". From Cybersecurity to Data Science to Full Stack development, all of these fields are possible applications of a Computer Science degree. We will give an overview of what each field entails and a bit about how the roles generally work together in a company.",
                 day: 5,
                 month: 2,
                 year: 2020,
             },
             {
-                loc: "Wednesday, February 12 from 7-8pm in Doyle Hall 001",
+                loc: "Wednesday, February 12 from 7-8pm in Cuneo",
                 description: "Chill. A relaxed meeting where you can come and work on any computer science projects that you might want to, try your hand at a coding challenge or two, and most importantly spend some time with your peers!",
                 day: 12,
                 month: 2,
                 year: 2020,
             },
             {
-                loc: "Wednesday, February 19 from 7-8pm in Doyle Hall 001",
+                loc: "Wednesday, February 19 from 7-8pm in Cuneo",
                 description: "Resume Workshop. There will be a presentation (mostly taken from Google, so you know it is legit!) Followed by peer discussion regarding your own resume and what could be added or taken off.",
                 day: 19,
                 month: 2,
                 year: 2020,
             },
             {
-                loc: "Wednesday, February 26 from 7-8pm in Doyle Hall 001",
+                loc: "Wednesday, February 26 from 7-8pm in Cuneo",
                 description: "Chill. A relaxed meeting where you can come and work on any computer science projects that you might want to, try your hand at a coding challenge or two, and most importantly spend some time with your peers!",
                 day: 26,
                 month: 2,
@@ -112,14 +112,14 @@ export default class Events extends React.Component {
                 year: 2020,
             },
             {
-                loc: "Wednesday, March 11 from 7-8pm in Doyle Hall 001",
+                loc: "Wednesday, March 11 from 7-8pm in Cuneo",
                 description: "Interview Workshop. Come try your hand at some technical interview questions to practice for your own future interviews! Open to all skill levels.",
                 day: 11,
                 month: 3,
                 year: 2020,
             },
             {
-                loc: "Wednesday, March 18 from 7-8pm in Doyle Hall 001",
+                loc: "Wednesday, March 18 from 7-8pm in Cuneo",
                 description: "Chill. A relaxed meeting where you can come and work on any computer science projects that you might want to, try your hand at a coding challenge or two, and most importantly spend some time with your peers!",
                 day: 18,
                 month: 3,
@@ -180,7 +180,7 @@ export default class Events extends React.Component {
         for (let i = 0; i < allEvents.length; i++) {
             if (yyyy > allEvents[i].year) {
                 past.unshift(
-                    <List.Item>
+                    <List.Item key={allEvents[i].loc}>
                         <i>{allEvents[i].loc}</i>
                         <br />
                         {allEvents[i].description}
@@ -191,7 +191,7 @@ export default class Events extends React.Component {
             }
             else if ((yyyy === allEvents[i].year) && (mm > allEvents[i].month)) {
                 past.unshift(
-                    <List.Item>
+                    <List.Item key={allEvents[i].loc}>
                         <i>{allEvents[i].loc}</i>
                         <br />
                         {allEvents[i].description}
@@ -202,7 +202,7 @@ export default class Events extends React.Component {
             }
             else if ((yyyy === allEvents[i].year) && (mm === allEvents[i].month) && (dd > allEvents[i].day)) {
                 past.unshift(
-                    <List.Item>
+                    <List.Item key={allEvents[i].loc}>
                         <i>{allEvents[i].loc}</i>
                         <br />
                         {allEvents[i].description}
@@ -213,7 +213,7 @@ export default class Events extends React.Component {
             }
             else {
                 upcoming.push(
-                    <List.Item>
+                    <List.Item key={allEvents[i].loc}>
                         <i>{allEvents[i].loc}</i>
                         <br />
                         {allEvents[i].description}
@@ -234,6 +234,7 @@ export default class Events extends React.Component {
                         </Grid.Column>
                         <Grid.Column id='list-column' width={8}>
                             <h2>Upcoming</h2>
+                            <p>Room numbers subject to change weekly - check Slack!</p>
                             <List bulleted>
                                 {upcoming}
                             </List>
@@ -247,15 +248,14 @@ export default class Events extends React.Component {
                 <Responsive maxWidth={991} as='div'>
                     <Image src={hackathonGroup} />
                     <br />
-                    <List bulleted id="mobile-list">
-                        <h2>Upcoming</h2>
-                        <List bulleted>
-                            {upcoming}
-                        </List>
-                        <h2>Past</h2>
-                        <List bulleted>
-                            {past}
-                        </List>
+                    <h2>Upcoming</h2>
+                    <p>Room numbers subject to change weekly - check Slack!</p>
+                    <List bulleted className="mobile-list">
+                        {upcoming}
+                    </List>
+                    <h2>Past</h2>
+                    <List bulleted className="mobile-list">
+                        {past}
                     </List>
                     <br />
                 </Responsive>
