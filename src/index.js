fc3cac2f8d0923dff50b41f8173c3e27726ce92e
Jonathan <warkentin.jon@gmail.com>
2020-05-16 15:26:52 -0500

    add comments on deployment stuff in index.html and index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import Routes from "./components/Routes"
 import { Router } from "react-router-dom"
 import { createBrowserHistory } from 'history'
 import 'semantic-ui-css/semantic.min.css'
-const history = createBrowserHistory({ basename: '/super-happiness' });
+// Comment out one of the 2 lines below depending on if you're deploying
+// to dev (GitHub pages) or production (Apache server)
+const history = createBrowserHistory({ basename: '/super-happiness' }); // for GH Pages
+// const history = createBrowserHistory({ basename: '/' }); // for Apache
 
 ReactDOM.render(
     <Router history={history}>
